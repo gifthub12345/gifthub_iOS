8207b827aaee9467e5f4acf103184b03b5ebd2e0
Sunho <sunh803@naver.com>
2024-05-05 15:28:57 +0900

    feat: make Project

---
+++ GiftHub/GiftHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftHubApp.swift
+//  GiftHub
+//
+//  Created by 235 on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct GiftHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
